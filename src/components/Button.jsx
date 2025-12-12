39bb33b63b4cc62a5f18545f01f49932bac70056
Nayan Sarania <nayan.sarania.work@gmail.com>
2025-12-12 12:14:01 +0530

    Install remixicon

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='text-white border border-white px-5 py-3 rounded-2xl'>Resume <i class="ri-arrow-right-up-line"></i></button>
+    </div>
+  )
+}
+
+export default Button
