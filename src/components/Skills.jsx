c224cb127c17e8e58c4a38ed6229649991ad1a15
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-01-26 11:59:25 +0530

    Update skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 const techStack = ['HTML', 'CSS', 'JavaScript', 'React', 'Tailwind', 'Supabase', 'Figma', 'Git', 'Github', 'NPM', 'Vite']
 
 const Skills = () => {
-  return (
-    <div className="flex flex-wrap gap-2 max-w-xl justify-center">
+  return <>
+    <h1 className='mt-10 mb-4 font-bold text-xl'>Skills & Technologies</h1>
+    <div className="flex flex-wrap gap-2 justify-cente">
+
       {techStack.map((skill) => {
         return (
           <div
             key={skill}
-            className='bg-blur/5 border border-accent rounded-full px-5 py-2 whitespace-nowrap'>
-            <p className='text-accent'>{skill}</p>
+            className='bg-blur/5 border border-gray-400 rounded-xl px-5 py-2 whitespace-nowrap'>
+            <p className='text-gray-400 hover:text-gray-200 transition-colors duration-300'>{skill}</p>
           </div>
         )
       })}
     </div>
-  )
+  </>
 }
 
 export default Skills
