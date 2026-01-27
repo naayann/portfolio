9e0cd3b551aabc93347ceed5708ccd4c0b06afde
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-01-27 13:14:36 +0530

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="mt-20 mb-6 text-center text-xs text-white/40">
+      <p>
+        Designed & Developed by {" "}
+          <span 
+            className='text-white/80 font-medium'>
+              NayanSarania
+          </span>
+      </p>
+      <p className='text-white/30 mt-1'>
+        &copy; 2026. All rights reserved.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
