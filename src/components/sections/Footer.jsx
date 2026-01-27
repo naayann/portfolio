bdd1b9685050788c4f3f9b82ab2bd7f47ba3dbd8
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-01-27 20:32:11 +0530

    Add quote above footer component

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="mt-20 mb-6 text-center text-xs text-white/40">
-      <p>
-        Designed & Developed by {" "}
-        <span
-          className='text-white/80 font-medium'>
-          NayanSarania
-        </span>
-      </p>
-      <p className='text-white/30 mt-1'>
-        &copy; 2026. All rights reserved.
-      </p>
-    </footer>
+    <>
+      {/* Quote */}
+      <div className="mt-24 mb-4 text-center">
+        <p className="italic text-white/50">
+          “Regret of time wasted is wasting more time.”
+        </p>
+      </div>
+
+      <footer className="mt-20 mb-6 text-center text-xs text-white/40">
+        <p>
+          Designed & Developed by {" "}
+          <span
+            className='text-white/80 font-medium'>
+            NayanSarania
+          </span>
+        </p>
+        <p className='text-white/30 mt-1'>
+          &copy; 2026. All rights reserved.
+        </p>
+      </footer>
+    </>
   )
 }
 
